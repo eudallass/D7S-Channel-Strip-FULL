faf41c10ff650d078ac9d31b5973935e2a1db96c
eudallass <thehooktrap@gmail.com>
2026-06-02 17:54:22 -0300

    ui: declare reference style module controls

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -5,7 +5,8 @@
 #include "PluginProcessor.h"
 #include "UI/RackModuleComponent.h"
 
-class D7SChannelStripFullAudioProcessorEditor : public juce::AudioProcessorEditor
+class D7SChannelStripFullAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                                private juce::Timer
 {
 public:
     explicit D7SChannelStripFullAudioProcessorEditor (D7SChannelStripFullAudioProcessor&);
@@ -17,6 +18,7 @@ public:
 private:
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     using ButtonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+    using ComboBoxAttachment = juce::AudioProcessorValueTreeState::ComboBoxAttachment;
 
     struct ParamSlider
     {
@@ -25,11 +27,25 @@ private:
         std::unique_ptr<SliderAttachment> attachment;
     };
 
+    struct ParamChoice
+    {
+        juce::Label label;
+        juce::ComboBox box;
+        std::unique_ptr<ComboBoxAttachment> attachment;
+    };
+
     void setupSlider (ParamSlider& control, const juce::String& labelText, const juce::String& paramID);
+    void setupChoice (ParamChoice& control, const juce::String& labelText, const juce::String& paramID, const juce::StringArray& items);
     void setupBypassButton (juce::ToggleButton& button, const juce::String& text, const juce::String& paramID, std::unique_ptr<ButtonAttachment>& attachment);
     void connectRackButton (RackModuleComponent& module, const juce::String& bypassParamID);
     void syncRackVisuals();
-    void layoutModuleControls (juce::Rectangle<int>& area, RackModuleComponent& module, std::initializer_list<ParamSlider*> sliders, juce::ToggleButton& bypassButton);
+    void timerCallback() override;
+
+    void layoutModuleControls (juce::Rectangle<int>& area,
+                               RackModuleComponent& module,
+                               std::initializer_list<ParamSlider*> sliders,
+                               std::initializer_list<ParamChoice*> choices,
+                               juce::ToggleButton& bypassButton);
 
     D7SChannelStripFullAudioProcessor& audioProcessor;
 
@@ -40,36 +56,64 @@ private:
     RackModuleComponent tube      { "D7S Tube" };
     RackModuleComponent esser     { "D7S Esser" };
 
+    ParamSlider rackInput;
+    ParamSlider rackOutput;
+    ParamSlider rackMix;
+    juce::Label rackMeterLabel;
+
     ParamSlider noiseSuppression;
+    juce::Label noiseMeterLabel;
     juce::ToggleButton noiseBypassButton;
     std::unique_ptr<ButtonAttachment> noiseBypassAttachment;
 
-    ParamSlider eqLow;
-    ParamSlider eqLowMid;
-    ParamSlider eqPresence;
-    ParamSlider eqAir;
+    ParamSlider eqHpf;
+    ParamSlider eqLpf;
+    ParamSlider eqLfFreq;
+    ParamSlider eqLfGain;
+    ParamSlider eqLmfFreq;
+    ParamSlider eqLmfGain;
+    ParamSlider eqLmfQ;
+    ParamSlider eqHmfFreq;
+    ParamSlider eqHmfGain;
+    ParamSlider eqHmfQ;
+    ParamSlider eqHfFreq;
+    ParamSlider eqHfGain;
+    juce::ToggleButton eqLfBellButton;
+    juce::ToggleButton eqHfBellButton;
     juce::ToggleButton eqBypassButton;
+    std::unique_ptr<ButtonAttachment> eqLfBellAttachment;
+    std::unique_ptr<ButtonAttachment> eqHfBellAttachment;
     std::unique_ptr<ButtonAttachment> eqBypassAttachment;
 
     ParamSlider comp76Input;
+    ParamSlider comp76Output;
     ParamSlider comp76Attack;
     ParamSlider comp76Release;
-    ParamSlider comp76Output;
+    ParamChoice comp76Ratio;
+    juce::Label comp76MeterLabel;
     juce::ToggleButton comp76BypassButton;
     std::unique_ptr<ButtonAttachment> comp76BypassAttachment;
 
     ParamSlider comp2aPeak;
     ParamSlider comp2aGain;
+    ParamSlider comp2aEmphasis;
+    ParamSlider comp2aMix;
+    ParamChoice comp2aMode;
+    juce::Label comp2aMeterLabel;
     juce::ToggleButton comp2aBypassButton;
     std::unique_ptr<ButtonAttachment> comp2aBypassAttachment;
 
-    ParamSlider tubeDrive;
-    ParamSlider tubeTone;
+    ParamSlider tubeBeauty;
+    ParamSlider tubeBeast;
+    ParamSlider tubeSensitivity;
     juce::ToggleButton tubeBypassButton;
     std::unique_ptr<ButtonAttachment> tubeBypassAttachment;
 
+    ParamSlider esserThreshold;
     ParamSlider esserFreq;
-    ParamSlider esserAmount;
+    ParamSlider esserRange;
+    ParamChoice esserMode;
+    juce::Label esserMeterLabel;
     juce::ToggleButton esserBypassButton;
     std::unique_ptr<ButtonAttachment> esserBypassAttachment;
 
