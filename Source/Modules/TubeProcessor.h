6d44e7b7cf6d430629e783acde01a479b75782f2
eudallass <thehooktrap@gmail.com>
2026-06-03 01:52:16 -0300

    tier1: add TubeProcessor interface

---
+++ Source/Modules/TubeProcessor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
+#include <array>
+#include "../Core/IModule.h"
+
+class TubeProcessor final : public IModule
+{
+public:
+    void prepare (double sampleRate, int blockSize, int numChannels) override;
+    void reset() override;
+    void process (juce::AudioBuffer<float>& buffer) override;
+    void process (juce::AudioBuffer<double>& buffer) override;
+    void setBypass (bool shouldBypass) override;
+    const juce::String getIdentifier() const override { return "tube"; }
+
+    void cacheParameters (juce::AudioProcessorValueTreeState& apvts);
+
+private:
+    template <typename FloatType>
+    void processInternal (juce::AudioBuffer<FloatType>& buffer);
+    static double onePoleCoeff (double frequency, double sampleRate) noexcept;
+
+    std::atomic<float>* beautyParam { nullptr };
+    std::atomic<float>* beastParam { nullptr };
+    std::atomic<float>* sensitivityParam { nullptr };
+    std::atomic<float>* mixParam { nullptr };
+    std::atomic<float>* bypassParam { nullptr };
+
+    double sr { 44100.0 };
+    int channels { 2 };
+    std::array<double, 8> beautyState {};
+    std::array<double, 8> beastState {};
+
+    juce::SmoothedValue<float> beautySmooth;
+    juce::SmoothedValue<float> beastSmooth;
+    juce::SmoothedValue<float> sensitivitySmooth;
+    juce::SmoothedValue<float> mixSmooth;
+    juce::SmoothedValue<float> bypassWet;
+};
