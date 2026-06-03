8a33688582e6b3a89caefa67e1923cbcb114509b
eudallass <thehooktrap@gmail.com>
2026-06-03 01:21:43 -0300

    core: prepare ModuleChain for seven module order

--- Source/Core/ModuleChain.h
+++ Source/Core/ModuleChain.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <array>
 #include <vector>
 #include "ModuleState.h"
 
 class ModuleChain
 {
 public:
+    static constexpr int maxModules = 7;
+
     ModuleChain()
     {
         resetToDefault();
@@ -14,13 +17,27 @@ public:
     void resetToDefault()
     {
         modules.clear();
-
         modules.push_back ({ ModuleType::NoiseGT1, true });
         modules.push_back ({ ModuleType::EQ4K,     true });
         modules.push_back ({ ModuleType::Comp76,   true });
         modules.push_back ({ ModuleType::Comp2A,   true });
         modules.push_back ({ ModuleType::Tube,     true });
         modules.push_back ({ ModuleType::Esser,    true });
+        modules.push_back ({ ModuleType::Delay,    true });
+    }
+
+    void setOrder (const std::array<ModuleType, maxModules>& newOrder)
+    {
+        for (int i = 0; i < maxModules; ++i)
+            modules[(size_t) i].type = newOrder[(size_t) i];
+    }
+
+    std::array<ModuleType, maxModules> getOrder() const
+    {
+        std::array<ModuleType, maxModules> order {};
+        for (int i = 0; i < maxModules; ++i)
+            order[(size_t) i] = modules[(size_t) i].type;
+        return order;
     }
 
     std::vector<ModuleState>& getModules()
