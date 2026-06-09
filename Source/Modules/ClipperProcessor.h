406fcfe2d501d62b640b245fb17f430132788649
eudallass <thehooktrap@gmail.com>
2026-06-09 04:22:06 -0300

    clipper: rewrite processor header for faithful FSC params

--- Source/Modules/ClipperProcessor.h
+++ Source/Modules/ClipperProcessor.h
@@ -14,33 +14,31 @@ public:
     void process (juce::AudioBuffer<float>& buffer) override;
     void process (juce::AudioBuffer<double>& buffer) override;
     void setBypass (bool shouldBypass) override;
+    int getLatencySamples() const noexcept override { return 0; }
     const juce::String getIdentifier() const override { return "clipper"; }
 
     void cacheParameters (juce::AudioProcessorValueTreeState& apvts);
 
 private:
     template <typename FloatType>
-    void processInternal (juce::AudioBuffer<FloatType>& buffer);
+    void processInternal (juce::AudioBuffer<FloatType>& buffer) noexcept;
 
-    static double hybridClip (double x, double shape, double bias) noexcept;
-    double processDcBlocker (int channel, double x) noexcept;
+    static float fsClip (float x, float thresholdLinear) noexcept;
+    float processDcBlocker (int channel, float x) noexcept;
 
-    std::atomic<float>* driveParam { nullptr };
-    std::atomic<float>* ceilingParam { nullptr };
-    std::atomic<float>* shapeParam { nullptr };
-    std::atomic<float>* mixParam { nullptr };
+    std::atomic<float>* thresholdParam { nullptr };
+    std::atomic<float>* preParam { nullptr };
+    std::atomic<float>* postParam { nullptr };
     std::atomic<float>* bypassParam { nullptr };
 
     double sr { 44100.0 };
     int channels { 2 };
-    double dcCoef { 0.0 };
-    double biasPhase { 0.0 };
-    std::array<double, 8> dcX1 {};
-    std::array<double, 8> dcY1 {};
+    float dcCoef { 0.0f };
+    std::array<float, 8> dcX1 {};
+    std::array<float, 8> dcY1 {};
 
-    juce::SmoothedValue<float> driveSmooth;
-    juce::SmoothedValue<float> ceilingSmooth;
-    juce::SmoothedValue<float> shapeSmooth;
-    juce::SmoothedValue<float> mixSmooth;
+    juce::SmoothedValue<float> thresholdLinearSmooth;
+    juce::SmoothedValue<float> preGainLinearSmooth;
+    juce::SmoothedValue<float> postGainLinearSmooth;
     juce::SmoothedValue<float> bypassWet;
 };
